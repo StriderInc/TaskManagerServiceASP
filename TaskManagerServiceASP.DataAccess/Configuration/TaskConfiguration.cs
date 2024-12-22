6cc4b58a5d8bc292b5f828930396c96041dcfa0e
Geb <korotkiygs@gmail.com>
2024-12-23 01:00:58 +0300

    add TaskConfiguration

---
+++ TaskManagerServiceASP.DataAccess/Configuration/TaskConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagerServiceASP.Core.Model;
+using TaskManagerServiceASP.DataAccess.Entity;
+
+namespace TaskManagerServiceASP.DataAccess.Configuration;
+
+public class TaskConfiguration: IEntityTypeConfiguration<TaskEntity>    
+{
+    public void Configure(EntityTypeBuilder<TaskEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Title)
+            .HasMaxLength(TaskModel.MAX_TITLE_LENGTH)
+            .IsRequired();
+    }
+}
