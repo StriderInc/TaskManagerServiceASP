46f869764fbc76a6d100ed684bc1fef032e9fdcf
Geb <korotkiygs@gmail.com>
2024-12-22 18:40:53 +0300

    add data access layer with db context

---
+++ TaskManagerServiceASP.DataAccess/TaskManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerServiceASP.Core.Model;
+
+namespace TaskManagerServiceASP.DataAccess;
+
+public class TaskManagerDbContext: DbContext
+{
+    public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    
+    public DbSet<TaskModel> Tasks { get; set; }
+}
