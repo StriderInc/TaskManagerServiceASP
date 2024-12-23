09e399bcfb1ab7d3bc2681c58487e8e5626afd52
Gleb <korotkiygs@gmail.com>
2024-12-23 18:56:38 +0300

    add swagger

--- TaskManagerServiceASP/Program.cs
+++ TaskManagerServiceASP/Program.cs
@@ -6,22 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
-var connectionString = builder.Configuration.GetConnectionString("TaskManagerDbContext");
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("MSSQL");
 builder.Services.AddDbContext<TaskManagerDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
 
 var app = builder.Build();
-app.MapGet("/", (TaskManagerDbContext db) => db.Tasks.ToList());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
