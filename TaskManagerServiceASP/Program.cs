08ffbbf61c288ddc8a4d2b348be7a6011228862d
Gleb <korotkiygs@gmail.com>
2024-12-23 19:40:32 +0300

    add swagger name

--- TaskManagerServiceASP/Program.cs
+++ TaskManagerServiceASP/Program.cs
@@ -24,7 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Manager");
         c.RoutePrefix = string.Empty;
     });
 }
